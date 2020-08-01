cf570363f3e5131f26a57591eda02112653d9e61
Semih Murat <smhmurat@gmail.com>
2020-08-02 02:05:03 +0300

    MenuView Design

--- SwiftUI-Menu-Design/ContentView.swift
+++ SwiftUI-Menu-Design/ContentView.swift
@@ -10,12 +10,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            
+            VStack {
+                MenuRow(menuTitle: "Hesap", menuIcon: "person.fill")
+                MenuRow(menuTitle: "Ayarlar", menuIcon: "gear")
+                MenuRow(menuTitle: "Yardım", menuIcon: "quote.bubble.fill")
+                MenuRow(menuTitle: "Çıkış Yap", menuIcon: "escape")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(radius: 30)
+            .padding(.horizontal, 30)
+        }
+        .padding(.bottom, 30)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MenuRow: View {
+    var menuTitle : String
+    var menuIcon : String
+    
+    var body: some View {
+        HStack (spacing: 16) {
+            Image(systemName: menuIcon)
+                .font(.system(size: 24, weight: .bold))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+            Text(menuTitle)
+                .font(.system(size: 24, weight: .bold, design: .default))
+                .frame(width: 120, alignment: .leading)
+        }
     }
 }
