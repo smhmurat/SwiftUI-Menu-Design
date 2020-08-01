3ef9da19cef4ae4087b8c03ca4e25edaa9df8e78
Semih Murat <smhmurat@gmail.com>
2020-08-02 01:44:53 +0300

    Initial Commit

---
+++ SwiftUI-Menu-Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Menu-Design
+//
+//  Created by semih on 2.08.2020.
+//  Copyright © 2020 Semih Murat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
